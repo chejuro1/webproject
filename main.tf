adfa385663bb5cb2e77182440bd02f10018c79c6
chejuro1 <46506190+chejuro1@users.noreply.github.com>
2020-07-04 00:10:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ provider "aws" {
   region = "us-east-2"
  
 #  shared_credentials_file = "./creds/serviceaccount.json"
+access_key = "${var.aws_access_key}"
+secret_key = "${var.aws_secret_key}"
 
 }
 
