43c26dab29c200ef8ea767cbe6456b71d898821b
chejuro1 <46506190+chejuro1@users.noreply.github.com>
2020-07-04 02:05:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
  
  #access_key = "${var.aws_access_key}"
  #secret_key = "${var.aws_secret_key}"
- credentials = "${file("./creds/serviceaccount.json")}"
+ shared_credentials_file = "./creds/serviceaccount.json"
 
 }
 
