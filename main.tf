63aa9fe1166de8edf7eed294baa01c1071b95bb9
chejuro1 <46506190+chejuro1@users.noreply.github.com>
2020-07-04 01:31:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@
 provider "aws" {
   region = "us-east-2"
  
- shared_credentials_file = "./aws/credentials/credential.txt"
-# access_key = "${var.aws_access_key}"
-# secret_key = "${var.aws_secret_key}"
+ shared_credentials_file = "/Users/jenkins/./aws/credentials"
+ #access_key = "${var.aws_access_key}"
+ #secret_key = "${var.aws_secret_key}"
 
 }
 
